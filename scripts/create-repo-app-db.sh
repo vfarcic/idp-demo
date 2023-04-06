3ee939f17a008bc667c246de60459d4227a1fe99
Viktor Farcic <viktor@farcic.com>
2023-04-07 01:33:00 +0200

    Initial commit

--- scripts/create-repo-app-db.sh
+++ scripts/create-repo-app-db.sh
@@ -7,13 +7,13 @@ DB_VERSION=$6
 DB_SIZE=$7
 
 echo "xxx"
-echo GITHUB_TOKEN $1
-echo GITHUB_USER $2
-echo APP $3
-echo IMAGE_REPO $4
-echo HOST $5
-echo DB_VERSION $6
-echo DB_SIZE $7
+echo GITHUB_TOKEN $GITHUB_TOKEN
+echo GITHUB_USER $GITHUB_USER
+echo APP $APP
+echo IMAGE_REPO $IMAGE_REPO
+echo HOST $HOST
+echo DB_VERSION $DB_VERSION
+echo DB_SIZE $DB_SIZE
 echo "xxx"
 
 curl -fsSL https://download.devstream.io/download.sh | bash
