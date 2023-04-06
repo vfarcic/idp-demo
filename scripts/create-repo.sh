cad50bb3959b735a136c5404bc10627d33e168c7
Viktor Farcic <viktor@farcic.com>
2023-04-06 03:20:44 +0200

    Initial commit

---
+++ scripts/create-repo.sh
@@ -0,0 +1,14 @@
+GITHUB_TOKEN=$1
+GITHUB_USER=$2
+APP=$3
+IMAGE_REPO=$4
+HOST=$5
+DB_VERSION=$6
+DB_SIZE=$7
+
+sh -c "$(curl -fsSL https://download.devstream.io/download.sh)"
+chmod +x dtm
+curl -o config.yaml https://raw.githubusercontent.com/vfarcic/template-go-backend-db-google/main/config.yaml
+./dtm init --config-file config.yaml
+./dtm apply --config-file config.yaml --yes
+rm config.yaml devstream.state
