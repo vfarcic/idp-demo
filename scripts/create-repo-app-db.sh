e6e2a64847c58ad285175b529c74119428d48155
Viktor Farcic <viktor@farcic.com>
2023-04-07 01:27:25 +0200

    Initial commit

--- scripts/create-repo-app-db.sh
+++ scripts/create-repo-app-db.sh
@@ -6,10 +6,20 @@ HOST=$5
 DB_VERSION=$6
 DB_SIZE=$7
 
+echo "xxx"
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo "xxx"
+
 curl -fsSL https://download.devstream.io/download.sh | bash
-ls -la
 chmod +x dtm
 curl -o config.yaml https://raw.githubusercontent.com/vfarcic/template-go-backend-db-google/main/config.yaml
+ls -la
 ./dtm init --config-file config.yaml
 ./dtm apply --config-file config.yaml --yes
 rm config.yaml devstream.state
