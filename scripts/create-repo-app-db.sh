fc1a06b4a907d250523238af16d23f6f3f950b1f
Viktor Farcic <viktor@farcic.com>
2023-04-09 13:18:22 +0200

    Initial commit

--- scripts/create-repo-app-db.sh
+++ scripts/create-repo-app-db.sh
@@ -6,8 +6,6 @@ export HOST=$5
 export DB_VERSION=$6
 export DB_SIZE=$7
 
-env
-
 curl -fsSL https://download.devstream.io/download.sh | bash
 chmod +x dtm
 curl -H "Cache-Control: no-cache, no-store" -o config.yaml https://raw.githubusercontent.com/vfarcic/template-go-backend-db-google/main/config.yaml
