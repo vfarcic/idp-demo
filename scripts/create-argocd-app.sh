bb95fd26a1b10dccde0cac58c179f787ecddc07c
Viktor Farcic <viktor@farcic.com>
2023-04-09 18:17:23 +0200

    Initial commit

--- scripts/create-argocd-app.sh
+++ scripts/create-argocd-app.sh
@@ -3,9 +3,6 @@ REPO_USER=$2
 REPO_NAME=$3
 ENVIRONMENT=$4
 
-# sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-# sudo chmod a+x /usr/local/bin/yq
-
 FILE_PATH=apps/${NAME}-${ENVIRONMENT}.yaml
 
 cp argocd/app-template.yaml $FILE_PATH
@@ -13,6 +10,3 @@ yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
 yq --inplace ".spec.source.repoURL = \"https://github.com/${REPO_USER}/${REPO_NAME}.git\"" $FILE_PATH
 yq --inplace ".spec.source.path = \"kustomize/overlays/${ENVIRONMENT}\"" $FILE_PATH
 yq --inplace ".spec.destination.namespace = \"${ENVIRONMENT}\"" $FILE_PATH
-
-# TODO: Remove
-cat apps/${NAME}-${ENVIRONMENT}.yaml
--- scripts/create-repo-app-db.sh
+++ scripts/create-repo-app-db.sh
@@ -1,15 +1,15 @@
 export GITHUB_TOKEN=$1
-export GITHUB_USER=$2
-export APP=$3
-export IMAGE_REPO=$4
-export HOST=$5
-export DB_VERSION=$6
-export DB_SIZE=$7
+export GITHUB_ORG=$2
+export GITHUB_USER=$3
+export APP=$4
+export IMAGE_REPO=$5
+export HOST=$6
+export DB_VERSION=$7
+export DB_SIZE=$8
 
 curl -fsSL https://download.devstream.io/download.sh | bash
 chmod +x dtm
 curl -H "Cache-Control: no-cache, no-store" -o config.yaml https://raw.githubusercontent.com/vfarcic/template-go-backend-db-google/main/config.yaml
-ls -la
 ./dtm init --config-file config.yaml
 ./dtm apply --config-file config.yaml --yes
 rm dtm config.yaml devstream.state
