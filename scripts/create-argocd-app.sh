a538d463460d4c865e1d5cc4cdcb7f96153f5c98
Viktor Farcic <viktor@farcic.com>
2023-04-09 19:02:31 +0200

    Initial commit

--- scripts/create-argocd-app.sh
+++ scripts/create-argocd-app.sh
@@ -1,5 +1,5 @@
 NAME=$1
-REPO_USER=$2
+REPO_ORG=$2
 REPO_NAME=$3
 ENVIRONMENT=$4
 
@@ -7,6 +7,6 @@ FILE_PATH=apps/${NAME}-${ENVIRONMENT}.yaml
 
 cp argocd/app-template.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
-yq --inplace ".spec.source.repoURL = \"https://github.com/${REPO_USER}/${REPO_NAME}.git\"" $FILE_PATH
+yq --inplace ".spec.source.repoURL = \"https://github.com/${REPO_ORG}/${REPO_NAME}.git\"" $FILE_PATH
 yq --inplace ".spec.source.path = \"kustomize/overlays/${ENVIRONMENT}\"" $FILE_PATH
 yq --inplace ".spec.destination.namespace = \"${ENVIRONMENT}\"" $FILE_PATH
